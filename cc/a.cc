1cdb580ca888a9b6011ccd3805d387cc28b29229
iamadal <mdadalkhan@gmail.com>
2022-11-02 23:17:15 +0600

    Initiated

--- cc/a.cc
+++ cc/a.cc
@@ -5,3 +5,8 @@ int main()
 	printf("Sample Coding from C\n");
 	return 0;
 }
+
+info operator+()
+{
+	
+}
---
+++ py/lib.py
@@ -0,0 +1,7 @@
+import time
+
+def name(name):
+	if name < 100:
+		print(name)
+	else
+	 print(i-1)
